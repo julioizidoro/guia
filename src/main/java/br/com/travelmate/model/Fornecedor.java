4a6864e82e02055ca8a6dcf6c192c830c285042c
jizid <jizid@DESKTOP-25COE3C>
2017-02-09 11:09:57 -0200

    Alteração no model fornecedor

--- src/main/java/br/com/travelmate/model/Fornecedor.java
+++ src/main/java/br/com/travelmate/model/Fornecedor.java
@@ -6,15 +6,12 @@
 package br.com.travelmate.model;
 
 import java.io.Serializable;
-import java.util.List;
 import javax.persistence.Basic;
-import javax.persistence.CascadeType;
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
-import javax.persistence.OneToMany;
 import javax.persistence.Table;
 import javax.validation.constraints.Size;
 
@@ -35,58 +32,16 @@ public class Fornecedor implements Serializable {
     @Size(max = 100)
     @Column(name = "nome")
     private String nome;
-    @Size(max = 50)
-    @Column(name = "pais")
-    private String pais;
-    @Size(max = 30)
-    @Column(name = "tipoLogradouro")
-    private String tipoLogradouro;
-    @Size(max = 100)
-    @Column(name = "logradouro")
-    private String logradouro;
-    @Size(max = 10)
-    @Column(name = "numero")
-    private String numero;
-    @Size(max = 50)
-    @Column(name = "complemento")
-    private String complemento;
-    @Size(max = 50)
-    @Column(name = "bairro")
-    private String bairro;
-    @Size(max = 50)
-    @Column(name = "cidade")
-    private String cidade;
-    @Size(max = 2)
-    @Column(name = "estado")
-    private String estado;
-    @Size(max = 9)
-    @Column(name = "cep")
-    private String cep;
-    @Size(max = 15)
-    @Column(name = "fone")
-    private String fone;
-    @Size(max = 18)
-    @Column(name = "cnpj")
-    private String cnpj;
-    // @Pattern(regexp="[a-z0-9!#$%&'*+/=?^_`{|}~-]+(?:\\.[a-z0-9!#$%&'*+/=?^_`{|}~-]+)*@(?:[a-z0-9](?:[a-z0-9-]*[a-z0-9])?\\.)+[a-z0-9](?:[a-z0-9-]*[a-z0-9])?", message="E-mail inválido")//if the field contains email address consider using this annotation to enforce field validation
-    @Size(max = 100)
-    @Column(name = "email")
-    private String email;
-    // @Max(value=?)  @Min(value=?)//if you know range of your decimal fields consider using these annotations to enforce field validation
-    @Column(name = "comissaoMatriz")
-    private Double comissaoMatriz;
-    @Column(name = "comissaoFranquia")
-    private Double comissaoFranquia;
-    @Size(max = 100)
     @Column(name = "logo")
     private String logo;
     @Column(name = "habilitarorcamento")
-    private Boolean habilitarorcamento;
-    @Size(max = 200)
+    private boolean habilitarorcamento;
     @Column(name = "site")
     private String site;
-    @OneToMany(cascade = CascadeType.ALL, mappedBy = "fornecedor")
-    private List<Fornecedorcidade> fornecedorcidadeList;
+    @Column(name = "anotarifario")
+    private int anotarifario;
+    @Column(name = "anotarifariocurso")
+    private int anotarifariocurso;
 
     public Fornecedor() {
     }
@@ -111,116 +66,12 @@ public class Fornecedor implements Serializable {
         this.nome = nome;
     }
 
-    public String getPais() {
-        return pais;
+    public int getAnotarifariocurso() {
+        return anotarifariocurso;
     }
 
-    public void setPais(String pais) {
-        this.pais = pais;
-    }
-
-    public String getTipoLogradouro() {
-        return tipoLogradouro;
-    }
-
-    public void setTipoLogradouro(String tipoLogradouro) {
-        this.tipoLogradouro = tipoLogradouro;
-    }
-
-    public String getLogradouro() {
-        return logradouro;
-    }
-
-    public void setLogradouro(String logradouro) {
-        this.logradouro = logradouro;
-    }
-
-    public String getNumero() {
-        return numero;
-    }
-
-    public void setNumero(String numero) {
-        this.numero = numero;
-    }
-
-    public String getComplemento() {
-        return complemento;
-    }
-
-    public void setComplemento(String complemento) {
-        this.complemento = complemento;
-    }
-
-    public String getBairro() {
-        return bairro;
-    }
-
-    public void setBairro(String bairro) {
-        this.bairro = bairro;
-    }
-
-    public String getCidade() {
-        return cidade;
-    }
-
-    public void setCidade(String cidade) {
-        this.cidade = cidade;
-    }
-
-    public String getEstado() {
-        return estado;
-    }
-
-    public void setEstado(String estado) {
-        this.estado = estado;
-    }
-
-    public String getCep() {
-        return cep;
-    }
-
-    public void setCep(String cep) {
-        this.cep = cep;
-    }
-
-    public String getFone() {
-        return fone;
-    }
-
-    public void setFone(String fone) {
-        this.fone = fone;
-    }
-
-    public String getCnpj() {
-        return cnpj;
-    }
-
-    public void setCnpj(String cnpj) {
-        this.cnpj = cnpj;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-
-    public void setEmail(String email) {
-        this.email = email;
-    }
-
-    public Double getComissaoMatriz() {
-        return comissaoMatriz;
-    }
-
-    public void setComissaoMatriz(Double comissaoMatriz) {
-        this.comissaoMatriz = comissaoMatriz;
-    }
-
-    public Double getComissaoFranquia() {
-        return comissaoFranquia;
-    }
-
-    public void setComissaoFranquia(Double comissaoFranquia) {
-        this.comissaoFranquia = comissaoFranquia;
+    public void setAnotarifariocurso(int anotarifariocurso) {
+        this.anotarifariocurso = anotarifariocurso;
     }
 
     public String getLogo() {
@@ -231,11 +82,11 @@ public class Fornecedor implements Serializable {
         this.logo = logo;
     }
 
-    public Boolean getHabilitarorcamento() {
+    public boolean isHabilitarorcamento() {
         return habilitarorcamento;
     }
 
-    public void setHabilitarorcamento(Boolean habilitarorcamento) {
+    public void setHabilitarorcamento(boolean habilitarorcamento) {
         this.habilitarorcamento = habilitarorcamento;
     }
 
@@ -247,12 +98,12 @@ public class Fornecedor implements Serializable {
         this.site = site;
     }
 
-    public List<Fornecedorcidade> getFornecedorcidadeList() {
-        return fornecedorcidadeList;
+    public int getAnotarifario() {
+        return anotarifario;
     }
 
-    public void setFornecedorcidadeList(List<Fornecedorcidade> fornecedorcidadeList) {
-        this.fornecedorcidadeList = fornecedorcidadeList;
+    public void setAnotarifario(int anotarifario) {
+        this.anotarifario = anotarifario;
     }
 
     @Override
@@ -264,7 +115,6 @@ public class Fornecedor implements Serializable {
 
     @Override
     public boolean equals(Object object) {
-        // TODO: Warning - this method won't work in the case the id fields are not set
         if (!(object instanceof Fornecedor)) {
             return false;
         }
@@ -279,5 +129,5 @@ public class Fornecedor implements Serializable {
     public String toString() {
         return "br.com.travelmate.model.Fornecedor[ idfornecedor=" + idfornecedor + " ]";
     }
-    
+
 }
